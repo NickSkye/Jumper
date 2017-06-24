a483ec1a4fb05e2e767a37a221532c6c6c1a9fe8
packerfan626 <dori_mouawad@hotmail.com>
2017-06-24 19:31:49 +0300

    Setting up implementation of profile screen

---
+++ Beetle/ProfileScreen.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileScreen.swift
+//  FlippysFlight
+//
+//  Created by Dori Mouawad on 6/24/17.
+//  Copyright © 2017 Muskan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileScreen: UIViewController {
+    
+}
